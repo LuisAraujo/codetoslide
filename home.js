c6e8de83fbee3e2d60795c6ffab46d94f527dac4
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2024-11-13 00:57:12 -0300

    add home page

---
+++ home.js
@@ -0,0 +1,10 @@
+btnCreate = document.getElementById("btn-create");
+btnCreate.addEventListener("click", function() {
+    window.open("gerate.html")
+});
+
+
+btnDoc = document.getElementById("btn-doc");
+btnDoc.addEventListener("click", function() {
+    window.open("doc.html")
+});
