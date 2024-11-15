8c8302dc4309d76348e9298d583abc083764a83d
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2024-11-15 00:02:58 -0300

    removing logo in home

--- text.py
+++ text.py
@@ -1,7 +1,15 @@
-#1-[title='changeState', description='Esta função modifica o estado do usuário (state é uma string)']
+##{"number": "4", "title":"changeState", "description":"Esta função modifica o estado do usuário (state é uma string)"}
 def changeState(state):
    mystate = statde
 
-#2-[title='Exibindo o estado', description='Esta função printa o estado do usuário no console.']
+##{"number":"3","title":"Exibindo o estado", "description":"Esta função printa o estado do usuário no console."}
 def printState():
-   print('o seu estado é', mystate)
+   print("o seu estado é", mystate)
+
+##{"number":"2","title":"changeState 2", "description":"Esta função modifica o estado do usuário (state é uma string)"}
+def changeState(state):
+   mystate = statde
+
+##{"number":"1","title":"Exibindo o estado 2", "description":"Esta função printa o estado do usuário no console."}
+def printState():
+   print("o seu estado é", mystate)
